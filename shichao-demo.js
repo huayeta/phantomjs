368fe13a2028ac2b67f829805b9547ce1fe04a73
zhuhui <zhuhui@lonlife.cn>
2016-12-09 13:38:29 +0800

    添加试潮

--- shichao-demo.js
+++ shichao-demo.js
@@ -1,17 +1,6 @@
-/**
- * phantomjs shike.js <垫>
- * 默认免邮费 美食
- * 搜索免邮费
- */
 
 var webpage=require('webpage');
 var system = require('system');
-
-var INDEX=1;
-var MAX=1;//最大多少页
-
-var keywords=['茶','乌龙茶','铁观音','蜂蜜','钢化膜','iphone+数据线','苹果+数据线','必备','苹果6+手机壳','iphone6+手机壳','沙发+垫','椅子+垫'];
-var kw;
 
 var page=webpage.create();
 page.settings.userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36';
@@ -25,8 +14,9 @@ page.onUrlChanged = function(targetUrl) {
       page.close();
       //获取
       console.log('开始获得地址');
-    //   start();
-    kwFn();
+      getList(function(){
+          finish();
+      });
   }
 };
 page.open('http://login.shikee.com/',function(status){
@@ -46,32 +36,7 @@ page.open('http://login.shikee.com/',function(status){
     // phantom.exit();
 })
 
-function kwFn(){
-    kw=keywords.shift();
-    console.log('关键词：'+kw);
-    INDEX=1;
-    start(function(){
-        if(keywords.length>0){
-            console.log('----------');
-            kwFn();
-        }else{
-            finish();
-        }
-    });
-}
-
-function start(cb){
-    console.log('开始抓取第'+INDEX+'页');
-    getList(INDEX,function(){
-        if(INDEX>=MAX){
-            return cb();
-        }
-        INDEX++;
-        start(cb);
-    })
-}
-
-function getList(index,cb){
+function getList(cb){
     var newPage=webpage.create();
     newPage.onUrlChanged=function(targetUrl){
         console.log(targetUrl);
@@ -79,27 +44,16 @@ function getList(index,cb){
     newPage.onConsoleMessage = function(msg) {
       console.log(msg);
     }
-    // var url='http://list.shikee.com/list-'+index+'.html?posfree=1&cate=5';
-    // if(system.args[1]){
-    //     var keyword=system.args[1];
-    //     var arr=system.args[1].split(':');
-    //     if(arr.length>1){
-    //         MAX=arr[1];
-    //         keyword=arr[0];
-    //     }
-    //     //如果存在搜索
-    //     url='http://list.shikee.com/list-'+index+'.html?posfree=1&keyword='+encodeURIComponent(keyword)
-    // }
-    url='http://list.shikee.com/list-'+index+'.html?posfree=1&keyword='+encodeURIComponent(kw)
+    var url='http://fashion.shikee.com/lists/index/?con_state=2';
     newPage.open(url,function(status){
       if(status!=='success')return console.log('失败');
        newPage.includeJs('http://static.shikee.com/common/js/jquery-1.10.2.min.js',function(){
            var urls=newPage.evaluate(function(){
-               var $box=$(document).find('.maxPicList .item');
+               var $box=$(document).find('.J_ImgLoad a');
                var urls=[];
                $box.each(function(){
                    var $this=$(this);
-                   urls.push($this.find('.pic').attr('href'));
+                   urls.push($this.attr('href'));
                })
                return urls;
            });
@@ -126,7 +80,7 @@ function getList(index,cb){
  */
  function delay(fn_arr,time){
      if(fn_arr.length==0){
-         return;
+        return;
      }
      var fn=fn_arr.pop();
      setTimeout(function(){
@@ -141,11 +95,7 @@ function createpage(url,OBJ,cb){
         // console.log(targetUrl);
     }
     newPage.onResourceRequested = function (req) {
-        //console.log('requested: ' + JSON.stringify(req, undefined, 4));
-        var url=JSON.parse(JSON.stringify(req, undefined, 4)).url;
-        if(url.indexOf('http://detail.shikee.com/detail/apply/')!=-1){
 
-        }
     }
     newPage.onConsoleMessage = function(msg) {
       console.log(msg);
@@ -154,27 +104,14 @@ function createpage(url,OBJ,cb){
       if(status!=='success')return console.log('失败');
       newPage.includeJs('http://static.shikee.com/common/js/jquery-1.10.2.min.js',function(){
           newPage.evaluate(function(){
-            if(window.Detail && window.Detail.apply){
-                Detail.apply();
-                //如果需要收藏
-                if($(document).find('.tip-collect').size()>0){
-                    Detail.wait_to_apply(5);
-                    setTimeout(function(){
-                        $('#linkCofirm').trigger('click');
-                    },5500)
-                }
-            }else{
-                console.log('不存在');
-            }
+            $('.J_TryBtn').click();
           });
-          setTimeout(function(){
-              OBJ.num--;
-              if(OBJ.num==0){
-                  cb();
-              }else{
-                  console.log('还剩下：'+OBJ.num);
-              }
-          },5500)
+          OBJ.num--;
+          if(OBJ.num==0){
+              cb();
+          }else{
+              console.log('还剩下：'+OBJ.num);
+          }
           setTimeout(function(){
               newPage.close();
         },6000)
